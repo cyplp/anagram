6e162d2a0d47242290658fd91189032dc7c0ac24
Cyprien Le Pannérer <cyplp@free.fr>
2019-03-30 13:49:39 +0100

    gets args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    let dict = args[0].clone();
+    let searching = args[1].clone();
+
+    println!("searching {} in {}", searching, dict);
 }
